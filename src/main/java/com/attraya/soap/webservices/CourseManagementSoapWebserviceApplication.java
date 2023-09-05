877241904d0fb8166ec86ec8b0fe1dbc436fc4e6
Attraya Das <attrayaghoshdas@gmail.com>
2023-09-05 22:23:25 +0530

    Initial Commit

---
+++ src/main/java/com/attraya/soap/webservices/CourseManagementSoapWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.attraya.soap.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementSoapWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementSoapWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attraya/soap/webservices/CourseManagementSoapWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attraya.soap.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementSoapWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
