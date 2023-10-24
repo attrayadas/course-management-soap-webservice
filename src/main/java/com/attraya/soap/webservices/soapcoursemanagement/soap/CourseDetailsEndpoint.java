6f17b5274ed9cee19a6995e4b84a8382ed6e2286
Attraya Das <attrayaghoshdas@gmail.com>
2023-10-24 14:36:55 +0530

    Implementing a service - Course Details Service - backend with in-memory

--- src/main/java/com/attraya/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/attraya/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,5 +1,8 @@
 package com.attraya.soap.webservices.soapcoursemanagement.soap;
 
+import com.attraya.soap.webservices.soapcoursemanagement.soap.bean.Course;
+import com.attraya.soap.webservices.soapcoursemanagement.soap.service.CourseDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -9,6 +12,9 @@ import com.attraya.courses.*;
 
 @Endpoint
 public class CourseDetailsEndpoint {
+
+	@Autowired
+	CourseDetailsService service;
 
 	// method
 	// input - GetCourseDetailsRequest
@@ -20,14 +26,16 @@ public class CourseDetailsEndpoint {
 	@PayloadRoot(namespace = "http://attraya.com/courses", localPart = "GetCourseDetailsRequest")
 	@ResponsePayload // to convert the response to XML
 	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+		Course course = service.findById(request.getId());
+		return mapCourse(course);
+	}
+	private GetCourseDetailsResponse mapCourse(Course course){
 		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
 		CourseDetails courseDetails = new CourseDetails();
-		courseDetails.setId(request.getId());
-		courseDetails.setName("Microservices Course");
-		courseDetails.setDescription("That would be a wonderful course!");
-
+		courseDetails.setId(course.getId());
+		courseDetails.setName(course.getName());
+		courseDetails.setDescription(course.getDescription());
 		response.setCourseDetails(courseDetails);
-
 		return response;
 	}
 }
